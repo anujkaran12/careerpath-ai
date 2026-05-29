9b35331a646d6d234be0cc18f37a87afca79f77d
Anuj Karan <anuj12082004dev@gmail.com>
2026-05-29 16:50:12 +0530

    feat: implemented roadmap schema

---
+++ backend/src/models/roadmap.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+
+const roadmapSchema = new Schema(
+  {
+    targetRole: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    currentSkills: {
+      type: [String],
+      default: [],
+    },
+
+    experienceLevel: {
+      type: String,
+      enum: ["Beginner", "Intermediate", "Advanced"],
+      required: true,
+    },
+
+    roadmap: [
+      {
+        step: Number,
+        title: String,
+        description: String,
+        estimatedDuration: String,
+        resources: [String],
+        projectSuggestion: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const RoadmapModel = mongoose.model("Roadmap", roadmapSchema);
