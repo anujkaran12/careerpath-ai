e4f92da52c7f4ad982165a218de5e274b3761705
Anuj Karan <anuj12082004dev@gmail.com>
2026-05-29 16:49:18 +0530

    fix: place the dotenv.config() on top for load all environment variables before all components imports

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config();
 import { connectDB } from "./config/db";
 import roadmapRouter from "./routes/roadmap.routes";
 
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+console.log(process.env.GEMINI_API_KEY);
 
 app.use(cors());
 app.use(express.json());
